da56ba3c36bb5a0616656b87e33a2912f53c155e
Kemal Bećarević <116683338+becarevicKemal22@users.noreply.github.com>
2024-09-16 02:15:10 +0200

    feat: implemented ANSI terminal compatibility check so that ANSI codes are not printed out when they are not supported

--- src/lexer/Keywords.h
+++ src/lexer/Keywords.h
@@ -16,22 +16,22 @@
  * Changing this map enables easy modification of the language's keywords without affecting any other code (except tests).
  */
 const std::unordered_map<std::wstring, TokenType> KEYWORDS = {
-        {L"ako", TokenType::If},
-        {L"inače", TokenType::Else},
-        {L"inace", TokenType::Else}, // double
-        {L"dok", TokenType::While},
-        {L"za", TokenType::For},
-        {L"tačno", TokenType::True},
-        {L"tacno", TokenType::True}, // double
-        {L"netačno", TokenType::False},
-        {L"netacno", TokenType::False}, // double
-        {L"null", TokenType::Null},
-        {L"ispiši", TokenType::Print},
-        {L"ispisi", TokenType::Print}, // double
-        {L"var", TokenType::Var},
-        {L"konst", TokenType::Const},
+        {L"ako",      TokenType::If},
+        {L"inače",    TokenType::Else},
+        {L"inace",    TokenType::Else}, // double
+        {L"dok",      TokenType::While},
+        {L"za",       TokenType::For},
+        {L"tačno",    TokenType::True},
+        {L"tacno",    TokenType::True}, // double
+        {L"netačno",  TokenType::False},
+        {L"netacno",  TokenType::False}, // double
+        {L"null",     TokenType::Null},
+        {L"ispiši",   TokenType::Print},
+        {L"ispisi",   TokenType::Print}, // double
+        {L"var",      TokenType::Var},
+        {L"konst",    TokenType::Const},
         {L"funkcija", TokenType::Function},
-        {L"vrati", TokenType::Return},
+        {L"vrati",    TokenType::Return},
 };
 
 #endif //BAKLAVA_KEYWORDS_H
